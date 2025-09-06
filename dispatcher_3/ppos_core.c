c9962e9d1a1e9aa0df535c926906f5d8708371fb
Ramon Pelle <ramon.pelle15@gmail.com>
2025-09-06 11:43:23 -0300

    p3: dispathcer + scheduler + ajustes funcoes p2 com queue

---
+++ dispatcher_3/ppos_core.c
@@ -0,0 +1,184 @@
+/*
+
+Nome: Ramon Pelle
+
+GRR: 20244408
+
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include "ppos.h"
+#include "../queue_0/queue.h"
+
+// #define DEBUG
+
+// ----------------
+#define START_ID 0
+#define STACKSIZE 64*1024	/* tamanho de pilha das threads (recuperado do contexts.c)*/
+
+// estados das tarefas
+#define CRIADA 0
+#define RODANDO 1
+#define SUSPENSA 2
+#define TERMINADA 3
+// ----------------
+
+void dispatcher();
+
+task_t *current_task, main_task, dispatcher_task;
+task_t *ready_q = NULL; // Fila de tarefas prontas 
+
+static int id_counter = START_ID;
+static int user_tasks = 0; // Contador de tarefas do usuário
+
+void ppos_init () {
+    /* desativa o buffer da saida padrao (stdout), usado pela função printf */
+    setvbuf (stdout, 0, _IONBF, 0);
+    
+    main_task.id = id_counter++;
+    main_task.prev = NULL;
+    main_task.next = NULL;
+    main_task.stack = NULL; 
+    main_task.status = CRIADA;
+
+    getcontext(&main_task.context);
+    current_task = &main_task;
+
+    user_tasks++; // main conta como uma tarefa de usuário
+
+    // Cria a tarefa dispatcher
+    task_init(&dispatcher_task, (void *)dispatcher, NULL);
+
+    #ifdef DEBUG
+    printf("ppos_init: main task inicializada. ID: %d\n", main_task.id);
+    #endif
+}
+
+int task_init (task_t *task, void  (*start_func)(void *), void *arg) {
+    if (!task || !start_func) return -1;
+
+    task->id = id_counter++;
+    task->prev = NULL;
+    task->next = NULL;
+    task->status = CRIADA;
+    task->stack = malloc(STACKSIZE);
+
+    // Inicializa contexto
+    getcontext(&task->context);
+    task->context.uc_stack.ss_sp = task->stack;
+    task->context.uc_stack.ss_size = STACKSIZE;
+    task->context.uc_stack.ss_flags = 0;
+    task->context.uc_link = 0;
+
+    makecontext(&task->context, (void (*)(void))start_func, 1, arg);
+
+    // Adiciona tarefa na fila
+    if (task != &dispatcher_task) {
+        queue_append((queue_t **)&ready_q, (queue_t *)task);
+        user_tasks++;
+    }
+
+    #ifdef DEBUG
+    printf("task_init: tarefa com ID %d criada\n", task->id);
+    #endif
+
+    return task->id;
+}
+
+int task_id () {
+    return current_task->id;
+}
+
+void task_exit (int exit_code) {
+    #ifdef DEBUG
+    printf("task_exit: encerrando tarefa ID %d com código %d\n", current_task->id, exit_code);
+    #endif
+    current_task->status = TERMINADA;
+    
+    // Se a tarefa que está sendo encerrada não for o dispatcher, transfere o controle para ele
+    if (current_task != &dispatcher_task) {
+        task_switch(&dispatcher_task);
+    } else {
+        task_switch(&main_task);
+    }
+}
+
+int task_switch (task_t *task) {
+    if (!task) return -1;
+    
+    task_t *prev_task = current_task;
+    
+    #ifdef DEBUG
+    printf("task_switch: trocando da tarefa ID: %d para a tarefa ID: %d\n", current_task->id, task->id);
+    #endif
+
+    current_task = task;
+    return swapcontext(&prev_task->context, &task->context);
+}
+
+task_t *scheduler() {
+#ifdef DEBUG
+    printf("scheduler: tarefas na fila: [");
+    if (ready_q)
+    {
+        task_t *aux = ready_q;
+        do
+        {
+            printf("%d", aux->id);
+            aux = aux->next;
+            if (aux != ready_q)
+                printf(", ");
+        } while (aux != ready_q);
+    }
+    printf("]\n");
+
+    if (ready_q) {
+        printf("scheduler: próxima tarefa escolhida: %d\n", ready_q->id);
+    }
+    #endif
+
+
+    return ready_q;
+}
+
+void dispatcher () {
+    #ifdef DEBUG
+    printf("dispatcher: iniciando, user_tasks: %d\n", user_tasks);
+    #endif
+
+    while (user_tasks > 0) {
+        task_t *next_task = scheduler();
+        if (!next_task) break; // Nenhuma tarefa pronta
+
+        task_switch(next_task);
+
+        #ifdef DEBUG
+        printf("dispatcher: tarefa atual ID %d\n", current_task->id);
+        #endif
+
+        if (next_task->status == TERMINADA) {
+            queue_remove((queue_t **)&ready_q, (queue_t *)next_task);
+            free(next_task->stack);
+            user_tasks--;
+            #ifdef DEBUG
+            printf("dispatcher: tarefa ID %d terminada e removida da fila\n", next_task->id);
+            #endif
+        } else {
+            next_task->status = RODANDO;
+        }
+
+    }
+    task_exit(0);
+}
+
+void task_yield () {
+    if (current_task == &dispatcher_task) return; // Dispatcher não deve ceder
+    
+    #ifdef DEBUG
+    printf("task_yield: tarefa ID %d cedendo o processador\n", current_task->id);
+    #endif
+
+    queue_remove((queue_t **)&ready_q, (queue_t *)current_task);
+    queue_append((queue_t **)&ready_q, (queue_t *)current_task);
+    task_switch(&dispatcher_task);
+}
